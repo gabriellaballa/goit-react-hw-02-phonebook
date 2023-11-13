8fe2ee184f69fdba1df85d76cb74af2704b9e6a4
gabriellaballa <balla_gabriella@yahoo.com>
2023-11-13 19:59:32 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -48,7 +48,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Phonebook</h1>
+        <h1>My Phonebook</h1>
         <div className="addformula">
           <ContactForm onAddContact={this.handleAddContact} />
           <label className="filterinput">
